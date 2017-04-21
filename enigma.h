df158b6c810b58dba988f56b0be2cdfd453057ac
Jack Danielski <jackdski555@gmail.com>
2017-04-21 16:00:37 -0700

    Update enigma.h

--- enigma.h
+++ enigma.h
@@ -16,36 +16,68 @@
 *
 */
 
-// Takes text from a file and uses it as the message being encrypted 
-// or decrypted. 
-void textEnter(); 
+typedef struct Enigmas; 
 
-// Takes text from stdin/keyboard and uses it as the message being 
-// encrypted or decrypted. 
-void stdinEnter();
+// Creates an Enigma type with name "name"
+Engima createEnigma(char name);
+
+// Delete an Enigma type
+// Returns -1 if Enigma n DNE
+// Returns 0 is successful
+int deleteEnigma(Enigma n); 
+
+// Print the values of Enigma n
+// Returns -1 if Enigma n DNE
+// Returns 0 if successful 
+int printEnigma(Enigma n);
+
+// Set enigma.message 
+void getMessage(Enigma enigma);
+
+// Get rid of spaces and punctuation 
+// Capitalize lowercase letters
+void formatMessage(Enigma enigma);
+
+// Set enigma.plugboard
+void setPlugboard(Enigma enigma, char plugOption);
+
+// Encrypts a letter according to its paired 
+// value on the Plugboard
+char encryptPlugboardChar(char c);
+
+// Encrypt according to the plugboard
+// using encryptPlugBoardChar function
+void encryptPlugboard(Enigma enigma);
+
+// Set enigma.ringstellung
+void setRingstellung(Engima enigma); 
+
+// Encrypt message according to ringstellung 
+void encryptRingstellung(Enigma enigma);
+
+// Set rotors in enigma 
+void setRotors(Enigma enigma);
+
+// Increment the proper rotors
+void incrementRotor(Enigma enigma, int number);
+
+// Encrypt a character according to the first rotor
+char encryptRotor(Enigma enigma, int number, char c);
+
+// Display help menu
+void help();
+
+// Displays Menu 1
+char menuOne();
+
+// Displays Menu 2
+// char menuTwo(); 
 
 // Sets off the process to encrypt
 void encrypt(); 
 
 // Sets off the process ot decrypt
 void decrypt();
-
-// Displays Menu 1
-void menuOne();
-
-// Displays Menu 2
-void menuTwo(); 
-
-// Display help menu
-void help();
-
-//Give the rotors values
-void setRotors(void);
-
-void menuOne() ;
-
-
-void menuTwo();
 
 /*
 void encrypt() {
@@ -58,7 +90,3 @@ void decrypt() {
 	setRotors();
 }
 */
-
-void help();
-
-void setRotors(void);
